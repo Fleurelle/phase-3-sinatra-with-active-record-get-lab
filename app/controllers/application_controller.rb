1d53f7df5142a34807950fcd548b787074a23b1e
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2022-03-11 14:32:05 -0500

    commit tree 6347b9c725478120b9c391d524ae752cf85d9d4e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
+  get '/bakeries' do
+    # binding.pry 
+    bakeries = Bakery.all 
+    bakeries.to_json 
+  end
+
+  get '/bakeries/:id' do
+    # binding.pry
+    bakeries_id = Bakery.find(params[:id])
+    bakeries_id.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do 
+    # binding.pry
+    bakeries_by_price = BakedGood.all.order(price: :desc)
+    bakeries_by_price.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    # binding.pry
+    expensive_baked_good = BakedGood.all.order(price: :desc).first
+    expensive_baked_good.to_json
+  end
 
 end
